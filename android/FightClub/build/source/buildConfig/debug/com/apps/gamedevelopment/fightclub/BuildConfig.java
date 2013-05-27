474d51cc8117323ba72cb5b09ecb201f488200a2
18rulez <prateekameta@prateekameta-macbookpro3.local>
2013-05-27 16:29:12 -0400

    First Comit

---
+++ android/FightClub/build/source/buildConfig/debug/com/apps/gamedevelopment/fightclub/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apps.gamedevelopment.fightclub;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
